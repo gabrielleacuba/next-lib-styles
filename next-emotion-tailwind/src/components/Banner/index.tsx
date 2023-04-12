80c12c3a9873834d48167ba8c4ed2e6b2e5eb66f
gabrielleacuba <gabrielleacuba@gmail.com>
2023-04-12 20:23:34 -0300

    create a project and add tokens

---
+++ next-emotion-tailwind/src/components/Banner/index.tsx
@@ -0,0 +1,9 @@
+import { BannerContainer, BannerGradient } from "./style";
+
+export const Banner = () => {
+  return (
+    <BannerContainer>
+      <BannerGradient />
+    </BannerContainer>
+  );
+};
